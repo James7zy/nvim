e0b9c6c061c4503dc5ab65e357d529aaa70ddf5c
James7zy <1036063809@qq.com>
2025-04-08 16:13:48 +0800

    telescope fuz search word

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -12,3 +12,14 @@ vim.keymap.set(
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
 
+-- 搜索当前光标的单词，精确匹配
+vim.keymap.set("n", "<leader>frb", function()
+    local word = vim.fn.expand("<cword>")
+    builtin.grep_string({ search = word })
+end)
+
+-- 搜索当前光标的单词，模糊匹配
+vim.keymap.set("n", "<leader>frc", function()
+    local word = vim.fn.expand("<cword>")
+    builtin.grep_string({ search = word, use_regex = true })
+end)
