1f1cf1133cb5dd9215882375e55b5aa254b3c4d8
James7zy <1036063809@qq.com>
2025-04-09 16:38:30 +0800

    CopilotChat Chat for nvim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -65,7 +65,6 @@ require("lazy").setup({
 		end,
 	},
 
-
 	-- easymotion find 
 	{
 		'easymotion/vim-easymotion',
@@ -167,4 +166,15 @@ require("lazy").setup({
 		end,
 	},
 
+	{
+		"CopilotC-Nvim/CopilotChat.nvim",
+		dependencies = {
+		  { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+		  { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+		},
+		build = "make tiktoken", -- Only on MacOS or Linux
+		opts = {
+		  -- See Configuration section for options
+		},
+	},
 })
