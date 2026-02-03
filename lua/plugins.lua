059490e0f22e989cdf5e45ea934a5f0c87175053
james7zy <1036063809@qq.com>
2026-02-03 13:30:36 +0800

    telescope bug fix

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -113,8 +113,11 @@ require("lazy").setup({
 	-- Fuzzy finder
 	{
 		"nvim-telescope/telescope.nvim",
-		branch = "0.1.x",
-		dependencies = { "nvim-lua/plenary.nvim" },
+		--branch = "0.1.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim" ,
+			"nvim-treesitter/nvim-treesitter"
+		},
 		config = function()
 			require("config.telescope")
 		end,
