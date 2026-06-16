6410f940d833efaa41ec056c002bc71d44181984
james7zy <1036063809@qq.com>
2026-06-16 12:44:51 +0800

    refactor(plugins): add plugins/ dir with misc.lua (faster + fidget)

---
+++ lua/plugins/misc.lua
@@ -0,0 +1,13 @@
+-- 零/极简配置的小插件
+return {
+  -- Improve the performance of big file
+  { "pteroctopus/faster.nvim" },
+
+  -- LSP progress UI
+  {
+    "j-hui/fidget.nvim",
+    opts = {
+      -- options
+    },
+  },
+}
