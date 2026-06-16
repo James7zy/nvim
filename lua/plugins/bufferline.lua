d4bf368a0b843299ca0b17bff6df4a7a65ffedd5
james7zy <1036063809@qq.com>
2026-06-16 13:49:04 +0800

    refactor(plugins): migrate tagbar/easymotion/highlighter/bufferline to plugins/

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,50 @@
+return {
+  'akinsho/bufferline.nvim',
+  version = "*",
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  config = function()
+    vim.opt.termguicolors = true
+    require("bufferline").setup {
+        options = {
+            -- left nvim-tree
+            offsets = {{
+                filetype = "NvimTree",
+                text = "File Explorer",
+                highlight = "Directory",
+                text_align = "left"
+            }},
+	mode = "buffers", -- buffer mode
+            numbers = "ordinal", -- show num
+            close_command = "bdelete! %d",       --close buffer
+            right_mouse_command = "bdelete! %d",
+            left_mouse_command = "buffer %d",    -- click left to close
+            middle_mouse_command = nil,
+            indicator = {
+              icon = '▎', style = 'icon',
+            },
+            buffer_close_icon = '',
+            modified_icon = '●',
+            close_icon = '',
+            left_trunc_marker = '',
+            right_trunc_marker = '',
+            diagnostics = "nvim_lsp",
+            separator_style = "slant", -- "slant" | "thick" | "thin"
+            show_buffer_close_icons = true,
+            show_close_icon = true,
+            enforce_regular_tabs = false,
+            always_show_bufferline = true,
+            sort_by = 'insert_after_current',
+        }
+    }
+
+    --keymap
+    local map = vim.keymap.set
+    map("n", "<Tab>", "<cmd>BufferLineCycleNext<CR>", { desc = "next buffer" })
+    map("n", "<S-Tab>", "<cmd>BufferLineCyclePrev<CR>", { desc = "before buffer" })
+
+    -- jump buffer
+    for i = 1, 9 do
+      map("n", "<leader>" .. i, "<cmd>BufferLineGoToBuffer " .. i .. "<CR>", { desc = "jump buffer" .. i })
+    end
+  end,
+}
---
+++ lua/plugins/easymotion.lua
@@ -0,0 +1,19 @@
+return {
+  'easymotion/vim-easymotion',
+  config = function()
+    -- <Leader>f{char} 跳转到字符 {char}
+    vim.api.nvim_set_keymap('n', '<Leader>s', '<Plug>(easymotion-bd-f)', { noremap = true, silent = true })
+    vim.api.nvim_set_keymap('n', '<Leader>so', '<Plug>(easymotion-overwin-f)', { noremap = true, silent = true })
+
+    -- s{char}{char} 跳转到字符 {char}{char}
+    vim.api.nvim_set_keymap('n', '<Leader>ss', '<Plug>(easymotion-overwin-f2)', { noremap = true, silent = true })
+
+    -- 跳转到行
+    vim.api.nvim_set_keymap('n', '<Leader>L', '<Plug>(easymotion-bd-jk)', { noremap = true, silent = true })
+    vim.api.nvim_set_keymap('n', '<Leader>L', '<Plug>(easymotion-overwin-line)', { noremap = true, silent = true })
+
+    -- 跳转到单词
+    vim.api.nvim_set_keymap('n', '<Leader>w', '<Plug>(easymotion-bd-w)', { noremap = true, silent = true })
+    vim.api.nvim_set_keymap('n', '<Leader>w', '<Plug>(easymotion-overwin-w)', { noremap = true, silent = true })
+  end,
+}
