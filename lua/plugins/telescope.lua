3e997f26c7c22341fe0a3711674c729ae049ea82
james7zy <1036063809@qq.com>
2026-06-16 13:42:48 +0800

    refactor(plugins): migrate telescope to plugins/ (inline config)

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,33 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  config = function()
+    local is_ok, builtin = pcall(require, "telescope.builtin")
+    if not is_ok then
+      return
+    end
+
+    vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+    vim.keymap.set("n", "<leader>ft", builtin.git_files, {})
+    vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+    vim.keymap.set("n", "<leader>fg", builtin.live_grep, {}) -- NOTE: requires ripgrep
+    vim.keymap.set("n", "<leader>fc", function() -- fc = find by content
+      builtin.grep_string({ search = vim.fn.input("rg > ") })
+    end)
+
+    -- 搜索当前光标的单词，精确匹配
+    vim.keymap.set("n", "<leader>frb", function()
+      local word = vim.fn.expand("<cword>")
+      builtin.grep_string({ search = word, word_match = "-w" })
+    end)
+
+    -- 搜索当前光标的单词，模糊匹配
+    vim.keymap.set("n", "<leader>frc", function()
+      local word = vim.fn.expand("<cword>")
+      builtin.grep_string({ search = word, use_regex = true })
+    end)
+  end,
+}
